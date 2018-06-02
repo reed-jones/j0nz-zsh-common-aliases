2386acdad69fe439f20b2f4b2093316c3cccbc5f
Reed Jones <reed.jones@pderas.com>
2018-06-02 17:43:01 -0600

    add nvm

--- common-aliases.plugin.zsh
+++ common-aliases.plugin.zsh
@@ -1,4 +1,6 @@
 export EDITOR=nvim
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Sync dotfiles repo
 alias dotfile='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
